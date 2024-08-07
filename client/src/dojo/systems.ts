d8d574165702d3164f5ca46a56a06271f7172d80
bal7hazar <bal7hazar@proton.me>
2024-08-07 16:35:18 -0400

    🎉 Start the client

---
+++ client/src/dojo/systems.ts
@@ -0,0 +1,118 @@
+import type { IWorld } from "./generated/contractSystems";
+import { toast } from "sonner";
+import * as SystemTypes from "./generated/contractSystems";
+import { ClientModels } from "./models";
+import { shortenHex } from "@dojoengine/utils";
+import { Account } from "starknet";
+
+export type SystemCalls = ReturnType<typeof systems>;
+
+export function systems({
+  client,
+  clientModels,
+}: {
+  client: IWorld;
+  clientModels: ClientModels;
+}) {
+  const TOAST_ID = "unique-id";
+
+  const extractedMessage = (message: string) => {
+    return message.match(/\('([^']+)'\)/)?.[1];
+  };
+
+  const isMdOrLarger = (): boolean => {
+    return window.matchMedia("(min-width: 768px)").matches;
+  };
+
+  const isSmallHeight = (): boolean => {
+    return window.matchMedia("(max-height: 768px)").matches;
+  };
+
+  const getToastAction = (transaction_hash: string) => {
+    return {
+      label: "View",
+      onClick: () =>
+        window.open(
+          `https://worlds.dev/networks/slot/worlds/zkchase/txs/${transaction_hash}`,
+        ),
+    };
+  };
+
+  const getToastPlacement = ():
+    | "top-center"
+    | "bottom-center"
+    | "bottom-right" => {
+    if (!isMdOrLarger()) {
+      // if mobile
+      return isSmallHeight() ? "top-center" : "bottom-center";
+    }
+    return "bottom-right";
+  };
+
+  const toastPlacement = getToastPlacement();
+
+  const notify = (message: string, transaction: any) => {
+    if (transaction.execution_status !== "REVERTED") {
+      toast.success(message, {
+        id: TOAST_ID,
+        description: shortenHex(transaction.transaction_hash),
+        action: getToastAction(transaction.transaction_hash),
+        position: toastPlacement,
+      });
+    } else {
+      toast.error(extractedMessage(transaction.revert_reason), {
+        id: TOAST_ID,
+        position: toastPlacement,
+      });
+    }
+  };
+
+  const handleTransaction = async (
+    account: Account,
+    action: () => Promise<{ transaction_hash: string }>,
+    successMessage: string,
+  ) => {
+    toast.loading("Transaction in progress...", {
+      id: TOAST_ID,
+      position: toastPlacement,
+    });
+    try {
+      const { transaction_hash } = await action();
+      toast.loading("Transaction in progress...", {
+        description: shortenHex(transaction_hash),
+        action: getToastAction(transaction_hash),
+        id: TOAST_ID,
+        position: toastPlacement,
+      });
+
+      const transaction = await account.waitForTransaction(transaction_hash, {
+        retryInterval: 100,
+      });
+
+      notify(successMessage, transaction);
+    } catch (error: any) {
+      toast.error(extractedMessage(error.message), { id: TOAST_ID });
+    }
+  };
+
+  const signup = async ({ account, ...props }: SystemTypes.Signup) => {
+    await handleTransaction(
+      account,
+      () => client.actions.signup({ account, ...props }),
+      "Player has been created.",
+    );
+  };
+
+  const rename = async ({ account, ...props }: SystemTypes.Rename) => {
+    await handleTransaction(
+      account,
+      () => client.actions.rename({ account, ...props }),
+      "Player has been renamed.",
+    );
+  };
+
+  return {
+    signup,
+    rename,
+  };
+}
---
+++ client/src/phaser/main.ts
@@ -0,0 +1,54 @@
+import { Boot } from "./scenes/Boot";
+import { Game } from "./scenes/Game";
+import { GameOver } from "./scenes/GameOver";
+import Phaser from "phaser";
+import { Preloader } from "./scenes/Preloader";
+import RexUIPlugin from "phaser3-rex-plugins/templates/ui/ui-plugin";
+
+export const app = {
+  width: 0,
+  height: 0,
+};
+
+const config = {
+  parent: "game-container",
+  dom: {
+    createContainer: true,
+  },
+  backgroundColor: 0x00a0b4,
+  banner: false,
+  type: Phaser.AUTO,
+  antialias: false,
+  disableContextMenu: true,
+  autoMobilePipeline: true,
+  autoRound: true,
+  scene: [Boot, Preloader, Game, GameOver],
+  scale: {
+    mode: Phaser.Scale.RESIZE,
+    width: "100%",
+    height: "100%",
+    min: {
+      width: 344,
+      height: 300,
+    },
+    max: {
+      width: 1920,
+      height: 816,
+    },
+  },
+  plugins: {
+    scene: [
+      {
+        key: "rexUI",
+        plugin: RexUIPlugin,
+        mapping: "rexUI",
+      },
+    ],
+  },
+};
+
+const StartGame = (parent: string) => {
+  return new Phaser.Game({ ...config, parent });
+};
+
+export default StartGame;
