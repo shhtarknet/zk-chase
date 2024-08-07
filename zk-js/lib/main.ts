e786f290f71f87f932563e78cec15259f5983c7c
Shramee Srivastav <shramee.srivastav@gmail.com>
2024-08-07 17:46:10 -0400

    proof verification

--- zk-js/lib/main.ts
+++ zk-js/lib/main.ts
@@ -6,7 +6,13 @@ import verification_key from "./fixtures/vk.json";
 export async function proove(inputs: any) {
 	const { proof, publicSignals } =
 		await snarkjs.groth16.fullProve(inputs, circuit_wasm_file, circuit_final_zkey_file);
-
-	console.log("Proof: ", proof);
+	console.log(publicSignals, proof);
+	const res = await snarkjs.groth16.verify(verification_key, publicSignals, proof);
+	console.log(res);
 	return { proof, publicSignals };
+}
+
+export async function verify(proof: any, publicSignals: any) {
+	console.log(publicSignals, proof);
+	return await snarkjs.groth16.verify(verification_key, publicSignals, proof);
 }
